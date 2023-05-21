7b831228cdd71dcd6635520ef326f80138f596e9
Appsstuff Solutions Company <appsstuff.com@gmail.com>
2023-05-21 21:47:31 +0300

    upload

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const appName = 'tabaa'
+
+// Serve only the static files form the dist directory
+app.use(express.static(`${__dirname}/dist/${appName}`));
+
+app.get('*', function (req, res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(`${__dirname}/dist/${appName}/index.html`));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3001);
